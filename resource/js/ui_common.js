6e289d48d45d18eac10eefff6d95f86b8ff7db63
tjdlwns33 <tjdlwns33@gmail.com>
2023-09-22 18:42:24 +0900

    230922 스타일, js작업

---
+++ resource/js/ui_common.js
@@ -0,0 +1,112 @@
+/*
+	전역변수
+*/
+var maincontH;//메인 컨텐츠 높이
+var scrT;//윈도우 스크롤탑
+
+
+
+/*
+	Load
+*/
+$(window).on('load', function(){
+    scrT = $(this).scrollTop();
+    maincontH = $('.main').find('.cont').outerHeight();
+
+    if ( scrT == 0 ){
+        mainVisualLoad();//메인비주얼 초기실행
+    }
+})
+
+
+
+/*
+	Scroll
+*/
+$(window).on('scroll', function(){
+    scrT = $(this).scrollTop();
+    
+    mainVisual();//메인비주얼
+})
+
+
+
+/*
+	스킵네비
+*/
+$(document).on('focus', '.skip-navi a', function() {
+	$(this).addClass('active');
+})
+$(document).on('blur', '.skip-navi a', function() {
+	$(this).removeClass('active');
+})
+//링크이동 시 영역 포커스
+$(document).on('click', '.skip-navi a', function() {
+	var skipId = $(this).attr('data-skipnaviId');
+    var $skipEl;
+	if( skipId == 'navi'){
+		$skipEl = $('.navi-area');
+	}
+	if ( skipId == 'container' ){
+		$skipEl = $('.container');
+	}
+	$skipEl.attr('tabindex','-1').focus();
+})
+
+
+
+/*
+	메인비주얼
+*/
+//메인비주얼 초기실행
+function mainVisualLoad(){
+    $('.main').find('.cont').eq(0).addClass('active');
+}
+
+
+function mainVisual(){
+    // bg
+    if ( scrT >= 0 && scrT <= maincontH){
+        $('.bg-wrap').css('top','50%').removeClass('nofix');
+    } else {
+        $('.bg-wrap').css('top',maincontH).addClass('nofix');
+    }
+
+    // cont
+    if ( scrT > maincontH/2 ){
+        $('.main').find('.cont').eq(0).removeClass('active');
+        $('.main').find('.cont').eq(1).addClass('active');
+
+        if(scrT >= maincontH) { 
+            $('.main').find('.cont').eq(1).addClass('nofix');
+        } else {
+            $('.main').find('.cont').eq(1).removeClass('nofix');
+        }
+    } else if ( scrT < maincontH/2 && scrT > 0 ) {
+        $('.main').find('.cont').eq(0).addClass('active');
+        $('.main').find('.cont').eq(1).removeClass('active');
+    }
+}
+
+
+
+/*
+	스크롤 탑버튼
+*/
+$(document).on('click', '.btn-top', function() {
+    $(window).scrollTop(0);
+    mainVisualLoad();////메인비주얼 초기실행
+})
+
+
+
+/*
+	네비게이션
+*/
+$(document).on('click', '.btn-navi', function() {
+    var thisIndex = $(this).index();
+    var $target = $('.content').eq(thisIndex);
+
+    $('html, body').stop().animate({ scrollTop : $target.offset().top }, 300);
+    $target.attr('tabindex','-1').focus();
+})
